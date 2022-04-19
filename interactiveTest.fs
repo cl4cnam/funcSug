652434f1e76e199b50c1ea12e7243ca5e03d74d1
cl4cnam <40176886+cl4cnam@users.noreply.github.com>
2022-04-19 20:37:42 +0200

    Update interactiveTest.fs

--- interactiveTest.fs
+++ interactiveTest.fs
@@ -1,2 +1,18 @@
 .print '.....> TEST'
+.print ''
+.print {seq 4 67 89}
+.var clickButton
+.var edit
 .print (&pluss 3 4 9)
+#(&inputText edit)
+#.print (&awaitHumanText)
+(&getClick theButton clickButton)
+.print '.....> MID'
+:await clickButton beep
+.print '.....> MID2'
+#.print .await edit
+.print :randomIntBetween 0 31
+(&displayMessage 'Hello!')
+#(&awaitClickBeep theButton)
+.print (&awaitHumanText)
+.print '.....> END'
