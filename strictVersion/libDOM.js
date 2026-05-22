fb047fc82358e1f58e4bbe74979e6c46ea377312
cl4cnam <40176886+cl4cnam@users.noreply.github.com>
2026-05-22 22:01:25 +0200

    Update libDOM.js

--- strictVersion/libDOM.js
+++ strictVersion/libDOM.js
@@ -2,29 +2,38 @@
 
 const $ = document.querySelector.bind(document) 
 
+function adaptString(p_text) {
+	const ls_text = p_text.replaceAll()
+	return ls_text
+}
+
 function outputHook(pState) {
-	const display = state.__display__
 	$('body').innerHTML = ''
-	if (display) {
-		for (const elt of display) {
-			const [ls_anchorSelector, ls_content] = elt.split('$$')
+	//~ console.warn(pState)
+	for (const [key, value] of pState) {
+		//~ if (key.genus === 'display') {
+		if (key.split('$$$')[0] === 'display') {
+			//~ const ls_anchorSelector = key.species
+			const ls_anchorSelector = key.split('$$$')[1]
 			const lElt_anchor = $(ls_anchorSelector)
-			lElt_anchor.insertAdjacentHTML('beforeend', ls_content)
+			//~ console.warn(value)
+			for (const elt of value) {
+				lElt_anchor.insertAdjacentHTML('beforeend', elt)
+			}
 		}
 	}
 }
 
 function inputHook(ps_genus, ps_species) {
-	if (ps_genus === '__seconds__') {
+	//~ console.log(ps_genus, ps_species)
+	if (ps_genus === 'seconds') {
 		setTimeout(function() {
 			runBurst({
 				type: {genus: ps_genus, species: ps_species},
 				value: undefined
 			})
 		}, ps_species * 1000)
-	} else if ( ps_genus.startsWith('__') && ps_genus.endsWith('__') ) {
-		const ls_evtType = ps_genus.slice(2, -2)
-		if (ls_evtType === 'textIsEntered') {
+	} else if (ps_genus === 'textIsEntered') {
 			const react = function(evt) {
 				if (evt.inputType==="insertParagraph") {
 					evt.target.removeEventListener('beforeinput', react)
@@ -34,7 +43,7 @@ function inputHook(ps_genus, ps_species) {
 					evt.preventDefault()
 					runBurst({
 						type: {genus: ps_genus, species: ps_species},
-						value: evt
+						value: evt.target.innerHTML
 					})
 				}
 			}
@@ -44,14 +53,14 @@ function inputHook(ps_genus, ps_species) {
 				$(ps_species).focus()
 				$(ps_species).addEventListener('beforeinput', react)
 			}, 0)
-		} else {
-			const react = function(evt) {
-				runBurst({
-					type: {genus: ps_genus, species: ps_species},
-					value: evt
-				})
-			}
-			$(ps_species).addEventListener(ps_genus.slice(2, -2), react, {once: true})
+	} else {
+		const react = function(evt) {
+			runBurst({
+				type: {genus: ps_genus, species: ps_species},
+				value: evt
+			})
 		}
+		//~ $(ps_species).addEventListener(ps_genus.slice(2, -2), react, {once: true})
+		$(ps_species).addEventListener(ps_genus, react, {once: true})
 	}
 }
